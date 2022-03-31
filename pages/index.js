98f5433bb286b34227d20293df94ddecd3f0f942
JeremyCarmona3 <jcarmona1@my.bcit.ca>
2022-03-30 21:08:54 -0700

    meta data and code clean up

--- pages/index.js
+++ pages/index.js
@@ -5,16 +5,15 @@ import styles from '../styles/Home.module.css'
 import Navbar from '../comps/global/navbar'
 import Title from '../comps/text/title'
 import Subtitle from '../comps/text/subtitle'
-import Text from '../comps/text/text'
 
 
 export default function Home() {
   return (
     <div className={styles.container}>
     <Head>
-      <title>Jeremy Carmona</title>
-      <meta name="keywords" content="Jeremy Carmona, Front-end Developer" />
-      <meta name="description" content="Jeremy Carmona's Portfolio" />
+      <title>Home</title>
+      <meta name="keywords" content="Jeremy Carmona Front-end Developer portfolio, BCIT Digital Design and Development (D3) 2022" />
+      <meta name="description" content="This portfolio includes projects in front-end development. Jeremy Carmona graduated from the D3 program at BCIT in 2022." />
       <meta name="author" content="Jeremy Carmona" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="icon" href="/favicon.ico" />
--- pages/projects.js
+++ pages/projects.js
@@ -2,19 +2,19 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-import TypeIt from 'typeit-react'
-
 import Navbar from '../comps/global/navbar'
 import Title from '../comps/text/title'
 import Subtitle from '../comps/text/subtitle'
-import Text from '../comps/text/text'
 
 export default function Projects() {
   return (
     <div>
       <Head>
         <title>My Projects</title>
-        <meta name="description" content="Jeremy Carmona's Portfolio" />
+        <meta name="keywords" content="Jeremy Carmona Front-end Developer portfolio, BCIT Digital Design and Development (D3) 2022" />
+        <meta name="description" content="This portfolio includes projects in front-end development. Jeremy Carmona graduated from the D3 program at BCIT in 2022." />
+        <meta name="author" content="Jeremy Carmona" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheets" href="/styles/globals.css" />
       </Head>
