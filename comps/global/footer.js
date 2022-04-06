bbc1a1758a8ad9de24d07672853260ec1bf8645d
JeremyCarmona3 <jcarmona1@my.bcit.ca>
2022-04-05 22:47:35 -0700

    footer changes

--- comps/global/footer.js
+++ comps/global/footer.js
@@ -9,7 +9,6 @@ const Cont = styled.div`
   justify-content: center;
   flex-wrap: wrap;
   min-height: 100px;
-  /* background-color: #fad; */
 `;
 
 
@@ -25,9 +24,12 @@ const Section = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  min-width: 400px;
-  /* flex: 1; */
-  /* border: 1px solid #000000; */
+  min-width: 300px;
+`;
+
+const BottomText = styled.p`
+  text-align: center;
+  font-size: 14px;
 `;
 
 export default function Footer({
@@ -38,23 +40,23 @@ export default function Footer({
     <Line />
     <Cont>
       <Section>
-        <FooterText onClick={() => router.push('https://www.youtube.com/channel/UCcmeyiH334i7tiCLhYIuc3g/featured')}>YouTube</FooterText>
-        <FooterText onClick={() => router.push('https://www.linkedin.com/in/jeremy-carmona-0685731bb/')}>LinkedIn</FooterText>
-        <FooterText onClick={() => router.push('https://github.com/JeremyCarmona3/')}>jeremycarmona115@gmail.com</FooterText>
-      </Section>
-      <Section>
         <FooterText onClick={() => router.push('/')}>Home</FooterText>
         <FooterText onClick={() => router.push('/about')}>About</FooterText>
+      </Section>
+      <Section>
         <FooterText onClick={() => router.push('/projects')}>Projects</FooterText>
         <FooterText onClick={() => router.push('https://github.com/JeremyCarmona3/')}>Github</FooterText>
       </Section>
       <Section>
-        <FooterText onClick={() => router.push('/copyright')}>Copyright</FooterText>
+        <FooterText onClick={() => router.push('https://www.linkedin.com/in/jeremy-carmona-0685731bb/')}>LinkedIn</FooterText>
+        <FooterText onClick={() => router.push('mailto:jeremycarmona115@gmail.com')}>Email</FooterText>
+      </Section>
+      <Section>
         <FooterText onClick={() => router.push('/termsofuses')}>Terms of uses</FooterText>
         <FooterText onClick={() => router.push('/privacypolicy')}>Privacy Policy</FooterText>
       </Section>
-      <p>&copy; Copyright 2022 Jeremy Carmona. All right{"'"}s reserved.</p>
     </Cont>
+    <BottomText>&copy; Copyright 2022 Jeremy Carmona. All right{"'"}s reserved.</BottomText>
   </div>
   )
 }
